b2c57175f2939e599e5cadfcba743935cc15215c
yiyu <yiyu@yiyus-MacBook-Air.local>
2025-08-23 15:50:58 +0800

    Add frontend html

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
-# create a Flask app
 app = Flask(__name__)
 
-# define a simple route
+# Home route
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return render_template("index.html")
 
-# run the app
 if __name__ == "__main__":
     app.run(debug=True)
-
